57ca0776a566954688f8c9e29cebbf0042216a6d
コウヘイトウバイ <touhei.beniume@gmail.com>
2020-05-05 04:59:44 +0900

    js追加

--- js/test.js
+++ js/test.js
@@ -1,9 +1,12 @@
 function init() {
     var canvas = document.getElementById('game');
-    var context = canvas.getContext('2d');
     
-    context.fillRect(20, 40, 50, 100);
+    if (canvas.getContext) {
+        var context = canvas.getContext("2d");
 
-    context.strokeStyle = 'rgb(0, 0, 0)';
-    context.fillStyle = 'rgb(255, 255, 255)';
+        context.fillRect(50, 50, 100, 100);
+
+        context.strokeStyle = "rgb(0, 0, 0)";
+        context.fillStyle = "rgb(0, 0, 0)";
+    }
 }
