153172f9ab4c77baeb0d80f485a8026ecb254b8e
コウヘイトウバイ <touhei.beniume@gmail.com>
2020-05-05 05:06:12 +0900

    test

--- js/test.js
+++ js/test.js
@@ -5,8 +5,8 @@ function init() {
         var context = canvas.getContext("2d");
 
         context.fillRect(50, 50, 100, 100);
-
-        context.strokeStyle = "rgb(0, 0, 0)";
-        context.fillStyle = "rgb(0, 0, 0)";
+    }
+    else {
+        alert("not canvas")
     }
 }
