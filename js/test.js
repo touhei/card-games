3bd9b8e52de316b3681ebe51c7e8715673aa7ce8
コウヘイトウバイ <touhei.beniume@gmail.com>
2020-05-05 05:19:22 +0900

    test

--- js/test.js
+++ js/test.js
@@ -1,12 +1,9 @@
 function init() {
+    document.write("test001");
+
     var canvas = document.getElementById('game');
     
     if (canvas.getContext) {
-        var context = canvas.getContext("2d");
-
-        context.fillRect(50, 50, 100, 100);
-    }
-    else {
-        alert("not canvas")
+        document.write("test002")
     }
 }
